06083a436b5dfa8b48454c95beee7e72dabb82ea
Carter Doss <dosscarter917@gmail.com>
2026-04-01 16:13:57 -0700

    Initial

---
+++ src/main/java/net/rubixkubezz/viewbobbingkeybind/ViewBobbingKeybind.java
@@ -0,0 +1,17 @@
+package net.rubixkubezz.viewbobbingkeybind;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ViewBobbingKeybind implements ModInitializer {
+	public static final String MOD_ID = "viewbobbingkeybind";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/rubixkubezz/viewbobbingkeybind/ViewBobbingKeybindClient.java
@@ -0,0 +1,12 @@
+package net.rubixkubezz.viewbobbingkeybind;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ViewBobbingKeybindClient implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
